0b4664b0ec5474daef87b3408919ae941aea68fb
manikwastika <manikwastikafors@gmail.com>
2026-03-27 13:46:50 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function app() {
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="title-content">
+            <h1>Daftar Belanjaan</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
